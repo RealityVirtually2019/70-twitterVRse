ab5a168628125babc680c5969125983bf9dc38a9
Simon Zirui Guo <simonguozirui@gmail.com>
2019-01-19 15:45:04 -0500

    prefab spawned and instaniated

--- TwitterVRse/Assets/Scripts/ExampleScript.cs
+++ TwitterVRse/Assets/Scripts/ExampleScript.cs
@@ -7,17 +7,53 @@ using UnityEngine.Windows.Speech;
 
 
 public class ExampleScript : MonoBehaviour {
-    public GameObject prefab;
+    public GameObject prefabBird;
 	// Use this for initialization
 	void Start () {
-        var obj1 = Instantiate(prefab);
-        obj1.transform.position = new Vector3(1, 1, 1);
-        var component = obj1.GetComponent<TextMesh>();
+        //Bird Group 1
+
+        var trend1Birds = new List<GameObject>();
+        // Intiazize the object and add it to the collection
+        //the code might not be pretty but since the locations are very precise to each bird, we did it one by one
+        //trend 1
+        var bird1 = Instantiate(prefabBird);
+        var bird2 = Instantiate(prefabBird);
+        var bird3 = Instantiate(prefabBird);
+        var bird4 = Instantiate(prefabBird);
+        var bird5 = Instantiate(prefabBird);
+        //trend 2
+        var bird6 = Instantiate(prefabBird);
+        var bird7 = Instantiate(prefabBird);
+        var bird8 = Instantiate(prefabBird);
+        var bird9 = Instantiate(prefabBird);
+        //trend 3
+        var bird10 = Instantiate(prefabBird);
+        var bird11= Instantiate(prefabBird);
+        var bird12 = Instantiate(prefabBird);
+        //trend 1
+        var bird13= Instantiate(prefabBird);
+        var bird14= Instantiate(prefabBird);
+        //trend 1
+        var bird15 = Instantiate(prefabBird);
+        trend1Birds.Add(bird1);
+        trend1Birds.Add(bird2);
+        trend1Birds.Add(bird3);
+        trend1Birds.Add(bird4);
+        trend1Birds.Add(bird5);
+        bird1.transform.position = new Vector3(1, 1, 1);
+        bird2.transform.position = new Vector3(4, 1, 1);
+        bird3.transform.position = new Vector3(7, 1, 1);
+        bird4.transform.position = new Vector3(10, 1, 1);
+        bird5.transform.position = new Vector3(13, 1, 1);
+
+
+        var component = bird1.GetComponent<TextMesh>();
         component.text = "Some string";
         StartDictation();
     }
-
-
+    
+    //Speech Recogntion using Cortana
+    //To trigger
     [SerializeField]
     private Text m_Hypotheses;
 
@@ -56,8 +92,8 @@ public class ExampleScript : MonoBehaviour {
         m_DictationRecognizer.Start();
 
     }
-        // Update is called once per frame
-        void Update () {
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
