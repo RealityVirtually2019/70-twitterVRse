900af7b4bcd2dc54b33c0432e7ded900bd262d95
Simon Zirui Guo <simonguozirui@gmail.com>
2019-01-20 12:54:42 -0500

    audio added

---
+++ TwitterVRse/Assets/Scripts/BirdTalkControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+
+public class BirdTalkControl : MonoBehaviour {
+
+
+ 
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch) >= 0.5)
+        {
+            Debug.Log("hit");
+        
+            SceneManager.LoadScene(sceneName: "BirdTweetTest");
+        }
+    }
+}
